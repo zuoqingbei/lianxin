79b534495d68c7436d4f061c4e6f5ca7f7c76c51
daoke_li <daoke_li@qq.com>
2017-11-22 13:16:24 +0800

    提交

--- src/main/webapp/static/js/flat.js
+++ src/main/webapp/static/js/flat.js
@@ -24,7 +24,26 @@ function tipResize() {
 }
 
 $(function () {
-    tipResize()
+    tipResize();
+    //从地图提示框内直接打开实验室曲线
+    $("body").on("click","#echartTips .echart_content>a",function () {
+        var centerId = $(this).data("centerid");
+        var $centerList = $('.lab .lab_content_l .switchBox>ul>li.noChildren,.lab .lab_content_l .switchBox>ul>li>ul>li', parent.document);
+        $centerList.each(function(index,elem){
+            if($(elem).data("centerid") === centerId){
+                var $headerBtn = $(elem).parents(".switchBox").prev().find("ul>li");
+                if($(elem).parents(".inland").length>0){
+                    $headerBtn.eq(0).click();
+                }else{
+                    $headerBtn.eq(1).click();
+                }
+                $(elem).click();
+                return false;
+            }
+        });
+
+    });
+
 });
 
 function getGeoArr(data) {
@@ -65,8 +84,35 @@ function createArrData(productCode, labType) {
                     return [p[0] + 100, p[1] + 100];
                 },
                 padding: [0, 0, 0, 0],
+                //            width: 207,
+                //            height: 110,
+                //            backgroundColor: 'rgba(13,43,67,0.7)',
+                //            borderColor: 'rgba(31,120,214,1)',
                 // params : 数组内容同模板变量，
                 formatter: function (param) {
+                    // console.log("------------------param:",param)
+                    /*                    var CountryName = param.name;
+                                        var $l3x3 = $("#l", parent.document);
+                                        $l3x3.siblings("#r,.labMain_content").hide().siblings(".labMain_content_country").show();
+                                        $l3x3.find(".legend-bottom li").removeClass('active');
+                                        parent.bgImgOff($l3x3.find(".legend-bottom li"));
+                                        console.log("show",CountryName);
+
+                                        switch (CountryName) {
+
+                                            case '日本研发中心':
+                                                window.parent.loadLabUnitInfoCenterTabAjaxWorld(0);
+                                                break;
+                                            case '新西兰研发中心':
+                                                window.parent.loadLabUnitInfoCenterTabAjaxWorld(2);
+                                                break;
+                                            case '泰国模块中心':
+                                                window.parent.loadLabUnitInfoCenterTabAjaxWorld(1);
+                                                break;
+                                            default:
+                                            console.log("暂无该国家实验室信息")
+                                        }
+                    */
                     //在这里是第一步
                     $elList = [];
                     //提示框的内容清空
@@ -204,7 +250,7 @@ function seriesData(data) {
             effect: {
                 show: true,
                 scaleSize: 1,
-                period: 30 ,
+                period: 5 * bodyScale,
                 color: '#ff0',
                 shadowBlur: 10 * bodyScale
             },
@@ -262,6 +308,7 @@ var myFlatMap = echarts.init($('.mapFlat')[0]);
 
 /*平面地图上点击各个国家的点，右侧切换到对应的实验室页面*/
 var $l3x3 = $("#l", parent.document);
+
 /*
 $("#echartTips").on("click", ".echart_tip_head", function () {
     var CountryName = $(this).parent().prev().find("a").attr("data-country");
@@ -298,13 +345,14 @@ function curvesSwitch(CountryName) {
             break;
         case ('泰国'):
             //window.parent.loadLabUnitInfoCenterTabAjaxWorld(1);
-            window.parent.loadLabUnitInfoCenterTabAjaxWorldHadoop(1,"thailand");
+            window.parent.loadLabUnitInfoCenterTabAjaxWorldHadoop(1, "thailand");
             break;
         default:
             console.log("暂无该国家实验室信息")
     }
-    console.log("国家名:",CountryName)
+    console.log("国家名:", CountryName)
 }
+
 var showTopicIndex = 0;
 /**
  * echartTips内的提示元素
@@ -453,8 +501,11 @@ function stopNewsShown($el) {
     // animation-play-state: paused
 }
 
-/*拼提示框的标签*/
+/**
+ * 拼提示框的标签
+ * */
 function getTopicHtml(currentPoint) {
+    // console.log("currentPoint",currentPoint)
     var city = "";
     if (currentPoint.name) {
         city = currentPoint.name;
@@ -465,18 +516,23 @@ function getTopicHtml(currentPoint) {
     var title = currentPoint.title;
     var id = currentPoint.id;
     var url = "";
+    var centerId = currentPoint.centerId;
+    var imgUrl = currentPoint.imgUrl;
     /*if((value=="0"&&title=="青岛")||title==undefined){
     	return "";
     }*/
     return $('<div class="echart_tip">' +
         '<div class="dialog_title echart_content">' +
-        '<a title="' + title + '" data-country="' + country + '"  href="#" target="_blank" >' +
-        '<span style="">' + title + '</span>' +
-        '</a>实验室数量：' + value +
+        '   <img src="'+ imgUrl +'" alt="实验室图片">' +
+        '   <div class="textBox">'  +
+            '   <h4 style="">' + title + '</h4>' +
+            '   <div>实验室数量：' + value + '</div>' +
+            '   <a data-centerId="'+centerId+'" href="javascript:void(0);">进入实验室 →</a>' +
+        '   </div>' +
         '</div>' +
         '<div class="echart_tip_arrow">' +
-        '<div class="echart_tip_line"></div>' +
-        '<div class="echart_tip_head"></div>' +
+        '   <div class="echart_tip_line"></div>' +
+        '   <div class="echart_tip_head"></div>' +
         '</div>' +
         '</div>');
 }
