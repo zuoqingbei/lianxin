83fb8844e70d918c45a763b43144985b65999fa8
daoke_li <daoke_li@qq.com>
2018-06-19 19:39:38 +0800

    按实验室颜色分类

--- src/main/webapp/static/js/hallMap.js
+++ src/main/webapp/static/js/hallMap.js
@@ -1,7 +1,6 @@
-function parentMethod(labCode) {//平面地图的数据加载
+function parentMethod(lineCode,labCode) {//平面地图的数据加载
     // $("#iframeFlat")[0].contentWindow.createArrData("21,22,23,24,25,26,27", "2,1,4,3");
-    $("#iframeFlat")[0].contentWindow.createArrData("", labCode);
-    return false;
+    $("#iframeFlat")[0].contentWindow.createArrData(lineCode, labCode);
 }
 // 激活
 function active($this){
@@ -9,10 +8,17 @@ function active($this){
 }
 $(function () {
 
-    // $(window.frames["iframeFlat"].document).find("");
+/*
+    //设置产线和实验室的颜色
+    $(".legend-m ul:eq(0)>li").each(function (index,item) {
+        if($(this).hasClass("active")){}
+    })
+*/
 
-    // a标签点击
-    $(".legend-m ul>li").addClass("active").click(function () {
+
+
+    let lineCode,labCode;
+    $(".legend-m ul:eq(0)>li").addClass("active").click(function () {
         active($(this));
         let labCode = $(this).index() + 1;
         if (labCode === 1) {
@@ -20,8 +26,13 @@ $(function () {
         } else if (labCode === 2) {
             labCode = 1;
         }
-        //重置地图数据
-        parentMethod(labCode);
+        parentMethod(lineCode,labCode);
+        return false;
+    });
+    $(".legend-m ul:eq(1)>li").addClass("active").click(function () {
+        active($(this));
+        let lineCode = $(this).index() + 21;
+        parentMethod(lineCode,labCode);
         return false;
     });
 
@@ -30,9 +41,22 @@ $(function () {
         active($(this));
         if ($(this).index() === 0) {
             $(".legend-m ul").css("top", "-1.3125em");
+            active($(".legend-m ul:eq(1)"))
         } else {
             $(".legend-m ul").css("top", "0");
+            active($(".legend-m ul:eq(0)"));
         }
+    })
+    //全选
+    $(".selectAll").click(function () {
+        $(".legend-m ul.active>li").addClass("active");
+        if($(this).index() ===0){
+            labCode = "2,1,3,4";
+        }else{
+            lineCode = "21,22,23,24,25,26,27";
+        }
+        parentMethod(lineCode,labCode);
+
     })
 
 
