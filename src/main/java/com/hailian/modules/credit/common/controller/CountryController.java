8e94fc34e8dc3491e6dd34680be65afcca0d0fec
doushuihai <dou_shuihai@163.com>
2018-08-23 09:53:49 +0800

    国家地区下拉框

--- src/main/java/com/hailian/modules/credit/common/controller/CountryController.java
+++ src/main/java/com/hailian/modules/credit/common/controller/CountryController.java
@@ -1,6 +1,7 @@
 package com.hailian.modules.credit.common.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import com.hailian.component.base.BaseProjectController;
 import com.hailian.jfinal.component.annotation.ControllerBind;
@@ -17,14 +18,13 @@ public class CountryController extends BaseProjectController {
 	/**
 	 * 国家地区下拉框  测试
 	* @author doushuihai  
+	 * @return 
 	* @date 2018年8月22日下午2:27:45  
 	* @TODO
 	 */
 	public void getCountrySelect(){
-		String countryName= getPara("countryName");//获取从前台查询国家地区的关键词查询条件参数
-		List<CountryModel> selectCountry = CountryService.service.selectCountry(countryName,this);
-		setAttr("countrylist",selectCountry);
-		System.out.println(selectCountry.size()+"=================selectCountry==================="+selectCountry);
+		Map<Object, Object> selectCountry = CountryService.service.CountrySelect(this);
+		renderJson(selectCountry);
 	}
 
 }
--- src/main/java/com/hailian/modules/credit/common/service/CountryService.java
+++ src/main/java/com/hailian/modules/credit/common/service/CountryService.java
@@ -1,7 +1,9 @@
 package com.hailian.modules.credit.common.service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -32,5 +34,21 @@ public class CountryService {
 		sql+="order by order_no";
 		return CountryModel.dao.find(sql,params.toArray());
 	
+	}
+	public Map<Object, Object> CountrySelect(BaseProjectController c) {
+		String sql="select continent,continent_en from credit_country where del_flag=0 group by continent";
+		List<CountryModel> continentList = CountryModel.dao.find(sql);
+		Map<Object,Object> map=new HashMap<Object, Object>();
+		for(CountryModel model:continentList){
+			List<Object> params=new ArrayList<Object>();
+			String innerSql="select name,name_en from credit_country where del_flag=0 and continent=?";
+			List<CountryModel> countrylist = CountryModel.dao.find(innerSql,params.toArray());
+			params.add(model.get("continent"));
+			List<String> continent = new ArrayList<String>();
+			continent.add((String) model.get("continent"));
+			continent.add((String) model.get("continent_en"));
+			map.put(continent, countrylist);
+		}
+		return map;
 	}
 }
