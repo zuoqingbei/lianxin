e22fa6ace5a25499ff814c44644510dbb5a4f452
doushuihai <dou_shuihai@163.com>
2018-08-29 09:26:49 +0800

    1

---
+++ src/main/java/com/hailian/modules/credit/utils/FileTypeUtils.java
@@ -0,0 +1,23 @@
+package com.hailian.modules.credit.utils;
+
+public class FileTypeUtils {
+	// 创建图片类型数组
+	public static String img[] = { "bmp", "jpg", "jpeg", "png", "tiff", "gif", "pcx", "tga", "exif", "fpx", "svg", "psd",
+	"cdr", "pcd", "dxf", "ufo", "eps", "ai", "raw", "wmf" };
+	// 创建图片类型数组
+	public static String document[] = { "doc", "docx", "xls","xlsx","pdf"};
+	public static boolean checkType(String ext){
+		boolean flag=false;
+		for (int i = 0; i < img.length; i++) {
+			if (img[i].equals(ext)) {
+				flag=true;
+			}
+		}
+		for (int i = 0; i < document.length; i++) {
+			if (document[i].equals(ext)) {
+				flag=true;
+			}
+		}
+		return flag;
+	}
+}
