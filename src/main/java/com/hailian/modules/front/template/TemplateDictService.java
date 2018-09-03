d5a599ffcbc548e2160792cf191ebf7244e9bf98
zuoqingbei <138898564@qq.com>
2018-09-03 18:47:30 +0800

    字典插件扩展

--- src/main/java/com/hailian/modules/front/template/TemplateDictService.java
+++ src/main/java/com/hailian/modules/front/template/TemplateDictService.java
@@ -3,6 +3,8 @@ package com.hailian.modules.front.template;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.hailian.jfinal.base.BaseService;
 import com.hailian.system.dict.DictCache;
 import com.hailian.system.dict.SysDictDetail;
@@ -35,6 +37,35 @@ public class TemplateDictService extends BaseService {
 		listDetail.add(allDict);
 		listDetail.addAll(DictCache.getSysDictDetailByType(type));
 		return listDetail;
+	}
+	
+	/**
+	 * 
+	 * @todo  根据dict_type获取字典
+	 * @time   2018年8月27日 下午5:06:56
+	 * @author zuoqb
+	 * @params
+	 */
+	public String getSysDictDetailSelect(String type,String selectedId) {
+		StringBuffer sb=new StringBuffer();
+		List<SysDictDetail> listDetail = new ArrayList<SysDictDetail>();
+		SysDictDetail allDict=new SysDictDetail();
+		allDict.set("detail_id", "");
+		allDict.set("dict_type",type);
+		allDict.set("detail_name","全部");
+		allDict.set("detail_name_en", "ALL");
+		allDict.set("detail_code", "");
+		listDetail.add(allDict);
+		listDetail.addAll(DictCache.getSysDictDetailByType(type));
+		for(SysDictDetail detail:listDetail){
+			if(StringUtils.isNotBlank(selectedId)&&selectedId.equals(detail.get("detail_id").toString())){
+				sb.append("<option selected='selected' m-detail-id='"+detail.get("detail_id")+"' m-detail-content='"+detail.get("detail_content")+"' m-detail-code='"+detail.get("detail_code")+"'  m-english='"+detail.get("detail_name_en")+"' value='"+detail.get("detail_id")+"'>"+detail.get("detail_name")+"</option>");
+			}else{
+				sb.append("<option m-detail-id='"+detail.get("detail_id")+"' m-detail-content='"+detail.get("detail_content")+"' m-detail-code='"+detail.get("detail_code")+"'  m-english='"+detail.get("detail_name_en")+"' value='"+detail.get("detail_id")+"'>"+detail.get("detail_name")+"</option>");
+			}
+			
+		}
+		return sb.toString();
 	}
 
 	
