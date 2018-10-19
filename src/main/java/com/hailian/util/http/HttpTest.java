de675fbd05f603eab57392a7e53e0f0e031b868f
goufan <goufan@goufandembp-2>
2018-10-19 14:08:18 +0800

    爬虫

--- src/main/java/com/hailian/util/http/HttpTest.java
+++ src/main/java/com/hailian/util/http/HttpTest.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
@@ -15,6 +16,7 @@ import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.htmlcleaner.CleanerProperties;
 import org.htmlcleaner.CompactXmlSerializer;
 import org.htmlcleaner.HtmlCleaner;
@@ -36,10 +38,10 @@ public class HttpTest {
 //				System.out.println("Local cookie: " + cookies.get(i));
 //			}
 //		}
-		File imageFile = new File("mofcomVerifyCode.jpeg");//图片位置
+		File imageFile = new File("mofcomVerifyCode.jpg");//图片位置
         ITesseract instance = new Tesseract();  // JNA Interface Mapping
         instance.setDatapath("C:\\Users\\Administrator\\Desktop\\tessdata");//设置tessdata位置
-        instance.setLanguage("osd");//选择字库文件（只需要文件名，不需要后缀名）
+        instance.setLanguage("eng");//选择字库文件（只需要文件名，不需要后缀名）
         String result = null;
 		try {
 			result = instance.doOCR(imageFile);
@@ -50,11 +52,39 @@ public class HttpTest {
 		}//开始识别
 		String name = "海尔集团";
 		String verifyCode = result;
-		//HttpClientUtils.sendGet("http://www.baidu.com","");
-		HttpClientUtils.sendGet("http://iecms.mofcom.gov.cn/pages/corp/NewCMvCorpInfoTabList.html", "sp=S&sp=S"+name+"&sp=S&sp=Ssearch&sp=S"+verifyCode);
+		//HttpClientUtils.sendGet("","");
+		System.out.println(getSource("http://iecms.mofcom.gov.cn/pages/corp/NewCMvCorpInfoTabList.html", "sp=S&sp=S"+name+"&sp=S&sp=Ssearch&sp=S"+verifyCode));
 		//HttpClientUtils.sendGet("http://wsjs.saic.gov.cn/txnS02.do", "y7bRbp=qmMEweVsmDl9_cN6OQYJvmxp_gbEk8.KgI.mouS16BABs9C4.A7NcK_F_zIxwkWtQHERRNxbxt8j469VFNXeZR4eMLZxKZkJiJ8sqy1gT5PRFQDP9hyeWue5nEOD1VxhiQeIt2mWemsaaD8_zKMPL_6BMVSiMeOe0vBRPh2SNDLS6GqX&c1K5tw0w6_=2d2XcxYZ7RcRNijUy8YPC6ahXg2of22SgU7RkwuP4m_o9um3vxueRoIrMYSXxpGJe3fnageWWOEiGpFVPs6sCfeCXX51Nrrf91urOx1zA9zOV7E.VZIjC5KgP6I4ov0Ga");
 		
     }
+	
+	public static String getSource(String url,String param) {
+	    String html = new String();
+	    HttpGet httpget = new HttpGet(url + "?" + param);     //创建Http请求实例，URL 如：https://cd.lianjia.com/
+	    // 模拟浏览器，避免被服务器拒绝，返回返回403 forbidden的错误信息
+	    httpget.setHeader("User-Agent", 
+	    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36");
+
+	    CloseableHttpResponse response = null;
+	    CloseableHttpClient httpclient = HttpClients.createDefault();   // 使用默认的HttpClient
+	    try {
+	        response = httpclient.execute(httpget);
+	        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {     // 返回 200 表示成功
+	            html = EntityUtils.toString(response.getEntity(), "utf-8");     // 获取服务器响应实体的内容
+	        }
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        if (response != null) {
+	            try {
+	                response.close();
+	            } catch (IOException e) {
+	                e.printStackTrace();
+	            }
+	        }
+	    }
+	    return html;
+	}
 	
 	/**
 	 * 爬取中国商标网
