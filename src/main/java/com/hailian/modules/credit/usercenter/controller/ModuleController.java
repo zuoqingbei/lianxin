8bed22d5dc31f55c280d5d1f6f61b146e7247f1b
yangdong <631730959@qq.com>
2018-10-25 16:02:07 +0800

    111

--- src/main/java/com/hailian/modules/credit/usercenter/controller/ModuleController.java
+++ src/main/java/com/hailian/modules/credit/usercenter/controller/ModuleController.java
@@ -1,5 +1,6 @@
 package com.hailian.modules.credit.usercenter.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.feizhou.swagger.annotation.Api;
@@ -9,6 +10,7 @@ import com.feizhou.swagger.annotation.Params;
 import com.hailian.component.base.BaseProjectController;
 import com.hailian.jfinal.component.annotation.ControllerBind;
 import com.hailian.modules.credit.reportmanager.model.CreditReportModuleConf;
+import com.hailian.modules.credit.usercenter.model.ModuleJsonData;
 @Api( tag = "用户控制台", description = "用户控制台" )
 @ControllerBind(controllerKey = "/credit/front/getmodule")
 public class ModuleController extends BaseProjectController{
@@ -20,8 +22,16 @@ public class ModuleController extends BaseProjectController{
 		})
 	public void list() {
 		String report=getPara("id");
-		List<CreditReportModuleConf> crmc=CreditReportModuleConf.dao.findReportNodes(report);
-		renderJson(crmc);
+		List<CreditReportModuleConf> crmcs=CreditReportModuleConf.dao.findByReport(report);
+		ModuleJsonData data=new ModuleJsonData();
+		List<ModuleJsonData> list=new ArrayList<ModuleJsonData>();
+		for(CreditReportModuleConf crmc:crmcs) {
+			List<CreditReportModuleConf> child=CreditReportModuleConf.dao.findParentNodes(crmc.get("id").toString(),report);
+			data.setContents(child);
+			data.setTitle(crmc.getStr("temp_name"));
+			list.add(data);
+		}
+		renderJson(list);
 	}
 	
 
---
+++ src/main/java/com/hailian/modules/credit/usercenter/model/ModuleJsonData.java
@@ -0,0 +1,23 @@
+package com.hailian.modules.credit.usercenter.model;
+
+import java.util.List;
+
+import com.hailian.modules.credit.reportmanager.model.CreditReportModuleConf;
+
+public class ModuleJsonData {
+	String title;
+	List<CreditReportModuleConf> contents;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public List<CreditReportModuleConf> getContents() {
+		return contents;
+	}
+	public void setContents(List<CreditReportModuleConf> contents) {
+		this.contents = contents;
+	}
+	
+}
