e6beabcaf6855d3b9e8115f3798d0977d35c670b
yangdong <631730959@qq.com>
2018-10-26 10:28:34 +0800

    模板接口

--- src/main/java/com/hailian/modules/credit/usercenter/controller/ModuleController.java
+++ src/main/java/com/hailian/modules/credit/usercenter/controller/ModuleController.java
@@ -9,8 +9,11 @@ import com.feizhou.swagger.annotation.Param;
 import com.feizhou.swagger.annotation.Params;
 import com.hailian.component.base.BaseProjectController;
 import com.hailian.jfinal.component.annotation.ControllerBind;
+import com.hailian.modules.admin.ordermanager.model.CreditCompanyInfo;
+import com.hailian.modules.admin.ordermanager.model.CreditOrderInfo;
 import com.hailian.modules.credit.reportmanager.model.CreditReportModuleConf;
 import com.hailian.modules.credit.usercenter.model.ModuleJsonData;
+import com.jfinal.plugin.activerecord.Record;
 @Api( tag = "用户控制台", description = "用户控制台" )
 @ControllerBind(controllerKey = "/credit/front/getmodule")
 public class ModuleController extends BaseProjectController{
@@ -21,14 +24,24 @@ public class ModuleController extends BaseProjectController{
 		@Param(name = "id", description = "报告id", required = false, dataType = "String"),
 		})
 	public void list() {
-		String report=getPara("id");
+		//订单id
+		String orederid=getPara("id");
+		//根据订单id获取订单信息
+		CreditOrderInfo coi=CreditOrderInfo.dao.findById(orederid);
+		//根据订单信息获取公司信息
+		CreditCompanyInfo cci=CreditCompanyInfo.dao.findById(coi.get("company_id"));
+		//根据订单信息获取报告id
+		String report=coi.get("report_type");
 		List<CreditReportModuleConf> crmcs=CreditReportModuleConf.dao.findByReport(report);
 		List<ModuleJsonData> list=new ArrayList<ModuleJsonData>();
 		for(CreditReportModuleConf crmc:crmcs) {
 			List<CreditReportModuleConf> child=CreditReportModuleConf.dao.findParentNodes(crmc.get("id").toString(),report);
 			list.add(new ModuleJsonData(crmc.getStr("temp_name"),child));
 		}
-		renderJson(list);
+		Record record=new Record();
+		record.set("company", cci);
+		record.set("modules",list);
+		renderJson(record);
 	}
 	
 
