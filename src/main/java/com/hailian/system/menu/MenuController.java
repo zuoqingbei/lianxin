925e8b937aebb29c3f4b5e6286c9f8997e9c0c8f
yangdong <631730959@qq.com>
2018-11-01 14:23:21 +0800

    111

--- src/main/java/com/hailian/system/menu/MenuController.java
+++ src/main/java/com/hailian/system/menu/MenuController.java
@@ -1,16 +1,29 @@
 package com.hailian.system.menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.feizhou.swagger.annotation.Api;
+import com.feizhou.swagger.annotation.ApiOperation;
+import com.feizhou.swagger.annotation.Param;
+import com.feizhou.swagger.annotation.Params;
 import com.hailian.component.base.BaseProjectController;
 import com.hailian.jfinal.component.annotation.ControllerBind;
 import com.hailian.jfinal.component.db.SQLUtils;
+import com.hailian.modules.credit.usercenter.model.ResultType;
+import com.hailian.system.menu.model.FristMenu;
+import com.hailian.system.menu.model.SecondMenu;
+import com.hailian.system.menu.model.ThirdMenu;
 import com.hailian.util.StrUtils;
 import com.jfinal.plugin.activerecord.Page;
+import com.jfinal.plugin.activerecord.Record;
 
 /**
  * 菜单
  * 
  * @author flyfox 2014-4-24
  */
+@Api(tag = "菜单路由", description = "菜单")
 @ControllerBind(controllerKey = "/system/menu")
 public class MenuController extends BaseProjectController {
 
@@ -53,6 +66,55 @@ public class MenuController extends BaseProjectController {
 		setAttr("parentSelect", svc.selectMenu(0));
 		render(path + "add.html");
 	}
+	/**
+	 * 
+	 * @time   2018年10月31日 下午3:33:55
+	 * @author yangdong 获取三级菜单下拉表
+	 * @todo   TODO
+	 * @param  
+	 * @return_type   void
+	 */
+	@ApiOperation(url = "/system/menu/add2",httpMethod="post", 
+			description = "获取订单列表")
+	@Params(value = { 
+		@Param(name = "", description = "", required = false, dataType = "Model"),
+		})
+	public void add2() {
+		//一级菜单列表
+		List<SysMenu> frist=SysMenu.dao.findFristMenu(0);
+		List<SysMenu> second=new ArrayList<SysMenu>();
+		List<SysMenu> third=new ArrayList<SysMenu>();
+	
+		List<FristMenu> fml=new ArrayList<FristMenu>();;
+		for(SysMenu sm:frist) {
+			//获取二级菜单
+			List<SecondMenu> sml=new ArrayList<SecondMenu>();
+			List<ThirdMenu> tml=new ArrayList<ThirdMenu>();
+			 second=SysMenu.dao.findFristMenu(sm.getInt("id"));
+			 if(second.size()<=0) {
+				 fml.add(new FristMenu(sm.getStr("name"),sm.getInt("id"))); 
+				 continue;
+			 }
+			for(SysMenu sm2:second) {
+				//获取3级菜单
+				third=SysMenu.dao.findFristMenu(sm2.getInt("id"));
+				if(third.size()<=0) {
+					sml.add(new SecondMenu(sm2.getStr("name"),sm2.getInt("id")));
+					continue;
+				}
+				for(SysMenu sm3:third) {
+					tml.add(new ThirdMenu(sm3.getStr("name"),sm3.getInt("id")));
+				}
+				 sml.add(new SecondMenu(sm2.getStr("name"),sm2.getInt("id"),tml));
+			}
+			
+			fml.add(new FristMenu(sm.getStr("name"),sm.getInt("id"),sml));
+			
+		}
+		fml.add(new FristMenu("根目录",0));
+		renderJson(fml);
+		
+	}
 
 	public void view() {
 		SysMenu model = SysMenu.dao.findById(getParaToInt());
@@ -75,7 +137,9 @@ public class MenuController extends BaseProjectController {
 	}
 
 	public void edit() {
-		SysMenu model = SysMenu.dao.findById(getParaToInt());
+		
+		
+		SysMenu model = SysMenu.dao.findById2(getParaToInt());
 		setAttr("parentSelect", svc.selectMenu(model.getInt("parentid")));
 		setAttr("model", model);
 		render(path + "edit.html");
