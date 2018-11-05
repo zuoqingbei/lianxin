3635bcbbbf19bf7f22d2c6ec0a9cd4c152a53ab3
leizhiguo <1348898564@qq.com>
2018-11-05 14:44:50 +0800

    优化

--- src/main/java/com/hailian/modules/credit/usercenter/controller/ReportInfoSaveData.java
+++ src/main/java/com/hailian/modules/credit/usercenter/controller/ReportInfoSaveData.java
@@ -2,7 +2,7 @@ package com.hailian.modules.credit.usercenter.controller;
 
 import com.hailian.jfinal.component.annotation.ControllerBind;
 
-@ControllerBind(controllerKey = "/credit/front/orderProcess")
+@ControllerBind(controllerKey = "/credit/front/ReportInfoSaveData")
 public class ReportInfoSaveData {
 	
 }
