6d5911b380e945b729b7d0093c053ab1d0fcb1a7
leizhiguo <1348898564@qq.com>
2018-11-05 10:50:31 +0800

    填报的动态生成

---
+++ src/main/java/com/hailian/modules/credit/usercenter/controller/ReportInfoSaveData.java
@@ -0,0 +1,8 @@
+package com.hailian.modules.credit.usercenter.controller;
+
+import com.hailian.jfinal.component.annotation.ControllerBind;
+
+@ControllerBind(controllerKey = "/credit/front/orderProcess")
+public class ReportInfoSaveData {
+	
+}
--- src/main/java/com/hailian/modules/credit/usercenter/model/ModuleJsonData.java
+++ src/main/java/com/hailian/modules/credit/usercenter/model/ModuleJsonData.java
@@ -7,23 +7,44 @@ import com.hailian.modules.credit.reportmanager.model.CreditReportModuleConf;
 
 public class ModuleJsonData {
 	String title="";
-	List<CreditReportModuleConf> contents=new ArrayList<CreditReportModuleConf>();
-	public ModuleJsonData(String title, List<CreditReportModuleConf> contents) {
-		this.title=title;
-		this.contents=contents;
-		
+	List<CreditReportModuleConf> contents = new ArrayList<CreditReportModuleConf>();
+	String smallModileType = "";
+	
+	public ModuleJsonData(String title, List<CreditReportModuleConf> contents, String smallModileType) {
+		super();
+		this.title = title;
+		this.contents = contents;
+		this.smallModileType = smallModileType;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public List<CreditReportModuleConf> getContents() {
 		return contents;
 	}
+
 	public void setContents(List<CreditReportModuleConf> contents) {
 		this.contents = contents;
+	}
+
+	public String getSmallModileType() {
+		return smallModileType;
+	}
+
+	public void setSmallModileType(String smallModileType) {
+		this.smallModileType = smallModileType;
+	}
+
+	@Override
+	public String toString() {
+		return "ModuleJsonData [title=" + title + ", contents=" + contents + ", smallModileType=" + smallModileType
+				+ "]";
 	}
 	
 }
