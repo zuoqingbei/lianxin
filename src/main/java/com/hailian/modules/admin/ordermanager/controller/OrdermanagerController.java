11c01c1140441ac522a6d29a713fea953a22e7d7
doushuihai <dou_shuihai@163.com>
2018-11-15 18:20:22 +0800

    为防止没有获取报告价格时遭到拦截，后台人为强行放值，上线是还原

--- src/main/java/com/hailian/modules/admin/ordermanager/controller/OrdermanagerController.java
+++ src/main/java/com/hailian/modules/admin/ordermanager/controller/OrdermanagerController.java
@@ -474,7 +474,13 @@ public class OrdermanagerController extends BaseProjectController{
 		}else {
 			price=OrderManagerService.service.getPrice(countryType,speed,reporttype,orderType);
 		}
-		renderJson(price);
+		/*
+		 * 为防止测试时拦截,上线后删掉
+		 */
+		CreditReportPrice test=new CreditReportPrice();
+		test.set("id", 99);
+		//renderJson(price);
+		renderJson(test);
 
 	}
 	/**
