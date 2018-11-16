564688dd6ad07c40af71d1ab413701413e3e7792
leizhiguo <1348898564@qq.com>
2018-11-16 14:13:22 +0800

    。。。。。。。。

--- src/main/java/com/hailian/modules/credit/usercenter/controller/finance/ExcelModule.java
+++ src/main/java/com/hailian/modules/credit/usercenter/controller/finance/ExcelModule.java
@@ -3,6 +3,7 @@ package com.hailian.modules.credit.usercenter.controller.finance;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.io.OutputStream;
 import java.lang.reflect.Method;
 import java.net.URLEncoder;
@@ -14,7 +15,6 @@ import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Font;
 import com.feizhou.swagger.annotation.Api;
 import com.hailian.component.base.BaseProjectController;
 import com.hailian.jfinal.component.annotation.ControllerBind;
@@ -23,14 +23,35 @@ import com.hailian.jfinal.component.annotation.ControllerBind;
 public class ExcelModule extends BaseProjectController  {
 	
 	public void  exportExcel() {
+		HttpServletResponse response = getResponse();
+		String fileName = "";
 		try {
-			OutputStream ops = getResponse().getOutputStream();
+			  fileName = new String("财务模板".getBytes("UTF-8"),"ISO-8859-1" );
+		} catch (UnsupportedEncodingException e1) {
+			fileName = "warning!";
+			e1.printStackTrace();
+		}
+		response.setHeader("Content-Disposition","attachment;filename="+fileName+".xlsx");//指定下载的文件名
+		//response.setContentType("application/vnd.ms-excel");
+		response.setContentType("multipart/form-data");
+		response.setCharacterEncoding("utf-8");
+		OutputStream ops = null;
+		try {
+			ops = response.getOutputStream();
 			HSSFWorkbook wb = new HSSFWorkbook();
-			wb.setSheetName(0, "阿斯顿发生");
-			wb.setSheetName(1, "啊啊啊");
+			wb.createSheet("阿斯顿发生");
+			wb.createSheet("二分无法");;
 			wb.write(ops);
 		} catch (IOException e) {
 			e.printStackTrace();
+		}finally {
+			if(ops!=null) {
+				try {
+					ops.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 	
