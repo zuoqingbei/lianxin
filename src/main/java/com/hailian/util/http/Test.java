9805548b2f2e0b74992120c49a426b9d554a9b33
doushuihai <dou_shuihai@163.com>
2018-12-01 14:27:04 +0800

    企查查

--- src/main/java/com/hailian/util/http/Test.java
+++ src/main/java/com/hailian/util/http/Test.java
@@ -1,57 +1,25 @@
 package com.hailian.util.http;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hailian.modules.admin.ordermanager.model.CreditCompanyCourtannouncement;
+import com.hailian.modules.admin.ordermanager.model.CreditCompanyJudgmentdoc;
+import com.hailian.modules.admin.ordermanager.model.CreditCompanyShareholder;
+
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//获取企业裁判文书
-		JSONObject caipanjson = HttpTest.getJudgmentDoc("阿里巴巴(中国)网络技术有限公司","");//获取api企业信息数据
-		String caipanstatus = caipanjson.getString("Status");
-		if("200".equals(caipanstatus)){
-			JSONObject Paging = caipanjson.getJSONObject("Paging");
-			int TotalRecords = Integer.parseInt(Paging.getString("TotalRecords"));
-			int PageSize = Integer.parseInt(Paging.getString("PageSize"));
-			int totalpage=1;
-			if(TotalRecords%PageSize==0){
-		           totalpage=TotalRecords/PageSize;
-			}else{
-		           totalpage=TotalRecords/PageSize+1;
-			}
-			for(int i=1;i<=totalpage;i++){
-				JSONObject json = HttpTest.getJudgmentDoc("阿里巴巴(中国)网络技术有限公司",i+"");//获取api企业裁判文书信息数据
-				System.out.println(json);
-				JSONArray jsonArray = json.getJSONArray("Result");
-				if(jsonArray !=null && jsonArray.size()>0){
-					for(int j=0;j<jsonArray.size();j++){
-						JSONObject judgmentdoc = (JSONObject)jsonArray.get(j);
-						String Id = judgmentdoc.getString("Id");//执行法院
-						String Court = judgmentdoc.getString("Court");//执行法院
-						String CaseName = judgmentdoc.getString("CaseName");//裁判文书名字   案件名称
-						String CaseNo = judgmentdoc.getString("CaseNo");//裁判文书编号   案件编号
-						String CaseType = judgmentdoc.getString("CaseType");//裁判文书类型
-						String SubmitDate = judgmentdoc.getString("SubmitDate");//发布时间
-						String UpdateDate = judgmentdoc.getString("UpdateDate");//审判时间
-						String IsProsecutor = judgmentdoc.getString("IsProsecutor");//是否原告
-						String IsDefendant = judgmentdoc.getString("IsDefendant");//是否被告（供参考）
-						String CourtYear = judgmentdoc.getString("CourtYear");//开庭时间年份
-						String CaseRole = judgmentdoc.getString("CaseRole");//案件身份
-						JSONObject judgmentdocdetail = HttpTest.getJudgmentDocDetail(Id);//获取api企业裁判文书详情
-						if(judgmentdocdetail.getString("Status").equals("200")){
-							String CaseReason=judgmentdocdetail.getJSONObject("Result").getString("CaseReason");//案由
-							JSONArray defendantjson =judgmentdocdetail.getJSONObject("Result").getJSONArray("Defendantlist");//被告
-							JSONArray prosecutorjson=judgmentdocdetail.getJSONObject("Result").getJSONArray("Prosecutorlist");//原告
-						}
-					}
-				}
-			}
-		}
+		
 		JSONObject courtannouncement = HttpTest.getCourtAnnouncement("阿里巴巴(中国)网络技术有限公司","");//获取api企业法院公告
 		String courtannouncementstatus = courtannouncement.getString("Status");
 		if(courtannouncementstatus.equals("200")){
-			JSONObject Paging = caipanjson.getJSONObject("Paging");
+			JSONObject Paging = courtannouncement.getJSONObject("Paging");
 			int TotalRecords = Integer.parseInt(Paging.getString("TotalRecords"));
 			int PageSize = Integer.parseInt(Paging.getString("PageSize"));
 			int totalpage=1;
@@ -63,6 +31,8 @@ public class Test {
 			for(int i=1;i<=totalpage;i++){
 				JSONObject CourtAnnouncementjson = HttpTest.getCourtAnnouncement("阿里巴巴(中国)网络技术有限公司",i+"");//获取api企业法院公告
 				JSONArray jsonArray = CourtAnnouncementjson.getJSONArray("Result");
+				 List<CreditCompanyCourtannouncement> list2 = JSONArray.toList(jsonArray, new CreditCompanyCourtannouncement(), new JsonConfig());//参数1为要转换的JSONArray数据，参数2为要转换的目标数据，即List盛装的数据
+				 List list3 = (List)JSONArray.toCollection(jsonArray, CreditCompanyCourtannouncement.class);  
 				for(int j=0;j<jsonArray.size();j++){
 					JSONObject CourtAnnouncement = (JSONObject)jsonArray.get(j);
 					String Id = CourtAnnouncement.getString("Id");//id
@@ -80,8 +50,8 @@ public class Test {
 		
 		JSONObject courtnotice = HttpTest.getCourtNotice("阿里巴巴(中国)网络技术有限公司","");//获取api企业法院公告
 		String courtnoticestatus = courtannouncement.getString("Status");
-		if(courtannouncementstatus.equals("200")){
-			JSONObject Paging = caipanjson.getJSONObject("Paging");
+		if(courtnoticestatus.equals("200")){
+			JSONObject Paging = courtnotice.getJSONObject("Paging");
 			int TotalRecords = Integer.parseInt(Paging.getString("TotalRecords"));
 			int PageSize = Integer.parseInt(Paging.getString("PageSize"));
 			int totalpage=1;
