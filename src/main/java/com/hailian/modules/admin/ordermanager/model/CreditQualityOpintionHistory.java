406ad718c6aa2c03bbc4cd620cc13b596557f54c
lixiaoyiyi <xiaoyi.li@hlsofttech.com>
2018-12-04 17:43:42 +0800

    质检意见历史表添加

---
+++ src/main/java/com/hailian/modules/admin/ordermanager/model/CreditQualityOpintionHistory.java
@@ -0,0 +1,10 @@
+package com.hailian.modules.admin.ordermanager.model;
+
+import com.hailian.component.base.BaseProjectModel;
+import com.hailian.jfinal.component.annotation.ModelBind;
+
+@ModelBind(table = "credit_quality_opintion_history")
+public class CreditQualityOpintionHistory extends BaseProjectModel<CreditQualityOpintionHistory>{
+	private static final long serialVersionUID = 1L;
+	public static final CreditQualityOpintionHistory dao = new CreditQualityOpintionHistory();//名字都叫dao，统一命名
+}
