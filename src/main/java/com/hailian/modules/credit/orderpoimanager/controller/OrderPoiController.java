ca68b1e0131db7015131619366c368047f0857e5
doushuihai <dou_shuihai@163.com>
2018-12-05 20:15:54 +0800

    批量订单调整

--- src/main/java/com/hailian/modules/credit/orderpoimanager/controller/OrderPoiController.java
+++ src/main/java/com/hailian/modules/credit/orderpoimanager/controller/OrderPoiController.java
@@ -259,7 +259,7 @@ public class OrderPoiController extends BaseProjectController {
 						CreditOrderInfo theSameOrder = OrderManagerService.service.isTheSameOrder(orderReal.get("right_company_name_en")+"",orderReal.get("report_type")+"",orderReal.get("report_language")+"", this);
 						if(theSameOrder!=null){
 							isTheSameOrderNum++;
-							isTheSameOrder+=isTheSameOrderNum+".第"+r+"行，第G列监测到有相同企业名称;";
+							isTheSameOrder+=isTheSameOrderNum+".第"+r+"行，第G列监测到有相同企业报告;";
 						}
 						//获取报告价格
 						if(orderReal.get("type") != null && orderReal.get("speed")!=null && orderReal.get("report_type")!= null  && orderReal.get("order_type")!=null && orderReal.get("custom_id")!=null && orderReal.get("country")!=null){
