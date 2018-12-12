5895cdfca25c089d3ea5ed445ecd28be6f8f3b5c
lyx <liyx@huazhenginfo.com>
2018-12-12 09:22:02 +0800

    客服  订单统计

--- src/main/webapp/static/credit/js/common.js
+++ src/main/webapp/static/credit/js/common.js
@@ -156,10 +156,15 @@ let Public = {
         })
     },
     goToOrderDetail(id, row) {
-        console.log("开始订单详情页跳转")
+        console.log("开始订单详情页跳转");
+        console.log(id);
+        console.log(row)
         //跳转订单详情
         $("#main_content").load(BASE_PATH + 'credit/front/home/orderInfo?id=' + id);
-        localStorage.setItem("row", JSON.stringify(row));
+        if(row){
+            localStorage.setItem("row", JSON.stringify(row));
+        }
+
     },
     createOrder() {
         $("#main_content").load(BASE_PATH + 'credit/front/home/createOrder')
