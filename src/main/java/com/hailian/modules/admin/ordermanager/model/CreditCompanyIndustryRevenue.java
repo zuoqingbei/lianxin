31c45a2d591c928dc320a274cfa304c1734d1ff4
goufan <496012561@qq.com>
2018-12-18 15:56:10 +0800

    bug修改

--- src/main/java/com/hailian/modules/admin/ordermanager/model/CreditCompanyIndustryRevenue.java
+++ src/main/java/com/hailian/modules/admin/ordermanager/model/CreditCompanyIndustryRevenue.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 @ModelBind(table = "credit_company_industry_revenue")
 //此标签用于模型与数据库表的连接
-public class creditCompanyIndustryRevenue extends BaseProjectModel<creditCompanyIndustryRevenue> {
+public class CreditCompanyIndustryRevenue extends BaseProjectModel<CreditCompanyIndustryRevenue> {
 	private static final long serialVersionUID = 1L;
-	public static final creditCompanyIndustryRevenue dao = new creditCompanyIndustryRevenue();//名字都叫dao，统一命名
+	public static final CreditCompanyIndustryRevenue dao = new CreditCompanyIndustryRevenue();//名字都叫dao，统一命名
 	public void deleteBycomIdAndLanguage(String companyid,String sys_language){
 		String sql="update credit_company_his set del_flag=1 where company_id=? and sys_language=?";
 		List<Object> params=new ArrayList<Object>();
