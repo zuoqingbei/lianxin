22d15dc9f2b9fd74f4a7477b3d6d7846f3fe74be
zhuchao <zhuchao1121@qq.com>
2018-12-27 18:00:16 +0800

    大数财务

--- src/main/webapp/static/credit/js/analyze/main.js
+++ src/main/webapp/static/credit/js/analyze/main.js
@@ -406,7 +406,10 @@ let ReportConfig = {
     	if(floatIndex.length === 0){return}
     	let cw_title = []
     	let cw_contents = []
+    	let ds_cw_title = []
+    	let ds_cw_contents = []
     	let cw_dom;
+    	let ds_dom;
     	_this.tableTitle = []
     	floatIndex.forEach((item,index)=>{
     		let floatParentId = this.floatTitle[index]['float_parent'];//浮动的父节点id
@@ -420,17 +423,91 @@ let ReportConfig = {
     					this.formIndex.push(i)
     					this.formTitle.push(this.floatTitle[index])
     				}else {
-    					//财务模块浮动
-//    					console.log(this.floatTitle[index])
-    					cw_title.push(this.floatTitle[index])
-    					cw_contents.push(this.floatContents[index])
-    					cw_dom = $("#titleCw"+i)
+    					if(_this.entityTitle[i]["get_source"].includes("type=3")){
+    						//大数财务模块浮动
+    						ds_cw_title.push(this.floatTitle[index])
+    						ds_cw_contents.push(this.floatContents[index])
+    						ds_dom = $("#titleDs"+i)
+    					}else {
+    						//财务模块浮动
+    						cw_title.push(this.floatTitle[index])
+    						cw_contents.push(this.floatContents[index])
+    						cw_dom = $("#titleCw"+i)
+    					}
     				}
     				
     			}
     		})
     	})
-    	console.log(cw_title,cw_contents)
+    	//大数财务逻辑
+    	let ds_top_html = ''
+		let ds_table_html = ''
+		ds_cw_title.forEach((item,index)=>{
+			//初始化大数财务模块
+			let this_content = ds_cw_contents[index];
+    		let moneySource = ds_cw_contents[0][0].get_source;
+    		let moneyStr = ''
+			let unitSource = ds_cw_contents[0][1].get_source;
+    		let unitStr = ''
+			$.ajax({
+				url:BASE_PATH + 'credit/front/ReportGetData/' + moneySource,
+				async:false,
+				type:'post',
+				success:(data)=>{
+					moneyStr = data.selectStr
+				}
+			})
+			$.ajax({
+				url:BASE_PATH + 'credit/front/ReportGetData/' + unitSource,
+				async:false,
+				type:'post',
+				success:(data)=>{
+					unitStr = data.selectStr
+				}
+			})
+			if(item.sort === 1) {
+				ds_top_html += `<div class="top-html mx-4">
+					<div class="d-flex justify-content-between align-items-center mt-4">
+						<!-- 单位 -->
+						<div class="ds-unit" style="width:100%">
+							<div class="form-inline my-3" >
+								<label style="font-weight:600;margin-left:60%" class="mr-3">${this_content[0].temp_name}</label>
+								<select class="form-control mr-3" id="${this_content[0].column_name}ds" style="width:10rem" name=${this_content[0].column_name}>${moneyStr}</select>
+								<select class="form-control mr-3" id="${this_content[1].column_name}ds" style="width:10rem" name=${this_content[1].column_name}>${unitStr}</select>
+							</div>
+						</div>
+					</div>
+					<div class="d-flex justify-content-between align-items-center mt-4">
+						<!-- 日期 -->
+						<div class="ds-date form-inline" style="width:100%">
+							<input class="form-control  my-3" id="${this_content[2].column_name}ds" style="margin-left:44%;margin-right:20%" type="text" name=${this_content[2].column_name}  placeholder=${this_content[2].place_hold} />
+							<input class="form-control"  id="${this_content[3].column_name}ds" type="text" name=${this_content[3].column_name}  placeholder=${this_content[3].place_hold} />
+						</div>
+					</div>`
+			}else {
+				ds_table_html += `<div class="table-content1 ds-table" style="background:#fff">
+									<table id="tableDs"
+										data-toggle="table"
+										style="position: relative"
+									>
+									</table>
+								</div>`
+			}
+		})
+		if(ds_dom){
+			ds_dom.after(ds_table_html)
+			ds_dom.after(ds_top_html)
+		}
+    	setTimeout(()=>{
+    		if(ds_cw_title[0]){
+    			InitObjAnalyze.bindDsConfig(ds_cw_title[0]['get_source'],_this.rows)
+    			InitObjAnalyze.initDsTable(ds_cw_contents[1],_this.dsGetSource,_this.dsAlterSource,_this.rows)
+    		}
+    	},0)
+    	
+    	
+    	//财务逻辑
+    	if(cw_title.length === 0){return}
     	this.cwConfigAlterSource = cw_title[0]['alter_source'];
     	this.cwConfigGetSource = cw_title[0]['get_source'];
     	let cw_top_html = ''
@@ -678,6 +755,8 @@ let ReportConfig = {
     	this.cwGetSource = '' //存放获取财务url
     	this.cwAlterSource = '' //存放修改财务url
     	this.cwDeleteSource = '' //删除财务url
+		this.dsGetSource = '' //存放大数获取财务url
+		this.dsAlterSource = '' //存放大数修改财务url
     	this.saveStatusUrl = ''
 		this.submitStatusUrl = ''
     	let row = localStorage.getItem("row");
@@ -782,10 +861,17 @@ let ReportConfig = {
                 			}
                 		}else if(smallModileType === '10'){
                 			//财务模块
-                			_this.cwGetSource = item.title.get_source;
-                			_this.cwAlterSource = item.title.alter_source;
-                			_this.cwDeleteSource = item.title.remove_source;
-                			contentHtml +=  `<div class="bg-f pb-4 mb-3 gjcw"><a class="l-title cwModal" name="anchor${item.title.id}" id="titleCw${index}">${item.title.temp_name}</a>`
+                			if(item["title"]["get_source"].includes("type=3")){
+                				//大数
+                				_this.dsGetSource = item.title.get_source;
+                				_this.dsAlterSource = item.title.alter_source;
+                				contentHtml +=  `<div class="bg-f pb-4 mb-3 gjds"><a class="l-title dsModal" name="anchor${item.title.id}" id="titleDs${index}">${item.title.temp_name}</a>`
+                			}else {
+                				_this.cwGetSource = item.title.get_source;
+                				_this.cwAlterSource = item.title.alter_source;
+                				_this.cwDeleteSource = item.title.remove_source;
+                				contentHtml +=  `<div class="bg-f pb-4 mb-3 gjcw"><a class="l-title cwModal" name="anchor${item.title.id}" id="titleCw${index}">${item.title.temp_name}</a>`
+                			}
                 		}else if(smallModileType !== '-2' && smallModileType !== '5' ) {
                 			contentHtml +=  `<div class="bg-f pb-4 mb-3"><a class="l-title" name="anchor${item.title.id}" id="title${index}">${item.title.temp_name}</a>`
                 		}
