c82fe5757a7da0186163a56ebc2702d45290f3b3
goufan <496012561@qq.com>
2019-01-22 17:47:19 +0800

    报告生成优化

--- src/main/java/com/hailian/util/word/MainReport.java
+++ src/main/java/com/hailian/util/word/MainReport.java
@@ -1,5 +1,6 @@
 package com.hailian.util.word;
 
+import com.hailian.api.constant.ReportTypeCons;
 import com.hailian.modules.admin.ordermanager.model.CreditOrderInfo;
 
 /**
@@ -13,36 +14,36 @@ public class MainReport {
         String reportType = order.getStr("report_type");
         String report_language = order.getStr("report_language");
 
-        //中文简体
-        if ("213".equals(report_language)) {
-            Roc102.reportTable(order, reportType, "612", userid);
-        } else if ("215".equals(report_language)) {
-            //英文
-            Roc102.reportTable(order, reportType, "613", userid);
-        } else if ("216".equals(report_language)) {
-            //中文简体+英文
-            if ("1".equals(reportType)) {
-                Roc102.reportTable(order, "1", "612", userid);
-                Roc102.reportTable(order, "7", "613", userid);
-            } else if ("8".equals(reportType)) {
-                Roc102.reportTable(order, "8", "612", userid);
-                Roc102.reportTable(order, "9", "613", userid);
-            } else if ("10".equals(reportType)) {
-                Roc102.reportTable(order, "10", "612", userid);
-                Roc102.reportTable(order, "11", "613", userid);
-            } else {
-                Roc102.reportTable(order, reportType, "", userid);
-            }
-        } else if ("217".equals(report_language)) {
+        if(ReportTypeCons.ROC_HY.equals(reportType)||ReportTypeCons.ROC_ZH.equals(reportType)||ReportTypeCons.ROC_EN.equals(reportType)) {
             //中文繁体+英文
-            if ("12".equals(reportType)) {
-                Roc102.reportTable(order, reportType, "613", userid);
+            if ("217".equals(report_language)) {
+                if ("12".equals(reportType) || "14".equals(reportType)) {
+                    Roc102.reportTable(order, reportType, "613", userid);
+                }
+            }else{
+                Roc102.reportTable(order, reportType, "612", userid);
             }
-            if("14".equals(reportType)){
-                Roc102.reportTable(order, reportType, "613", userid);
+        }else{
+            if ("213".equals(report_language)) {
+                BaseBusiCrdt.reportTable(order, reportType, "612", userid);
+            }else if ("215".equals(report_language)) {
+                //英文
+                BaseBusiCrdt.reportTable(order, reportType, "613", userid);
+            } else if ("216".equals(report_language)) {
+                //中文简体+英文
+                if ("1".equals(reportType)) {
+                    BaseBusiCrdt.reportTable(order, "1", "612", userid);
+                    BaseBusiCrdt.reportTable(order, "7", "613", userid);
+                } else if ("8".equals(reportType)) {
+                    BaseBusiCrdt.reportTable(order, "8", "612", userid);
+                    BaseBusiCrdt.reportTable(order, "9", "613", userid);
+                } else if ("10".equals(reportType)) {
+                    BaseBusiCrdt.reportTable(order, "10", "612", userid);
+                    BaseBusiCrdt.reportTable(order, "11", "613", userid);
+                } else {
+                    BaseBusiCrdt.reportTable(order, reportType, "", userid);
+                }
             }
-        } else {
-            Roc102.reportTable(order, reportType, "", userid);
         }
 
     }
