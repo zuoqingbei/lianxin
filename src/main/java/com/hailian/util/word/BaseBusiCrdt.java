660cb20ee7894260be61a5d0a740728394592c8d
goufan <496012561@qq.com>
2019-02-15 11:26:34 +0800

    财务模块修改

--- src/main/java/com/hailian/util/word/BaseBusiCrdt.java
+++ src/main/java/com/hailian/util/word/BaseBusiCrdt.java
@@ -400,7 +400,7 @@ public class BaseBusiCrdt {
         }
 
         //财务模块生成
-        String excelPath = "";
+        List<String> excelPath = new ArrayList<>();
         List<CreditCompanyFinancialStatementsConf> finanConfList = CreditCompanyFinancialStatementsConf.dao.findByWhere(" where company_id=? and del_flag=0 ",companyId);
         if(finanConfList!=null && finanConfList.size()>0) {
             CreditCompanyFinancialStatementsConf statementsConf = finanConfList.get(0);
@@ -408,21 +408,22 @@ public class BaseBusiCrdt {
             String end = statementsConf.get("date2");
             String finanId = statementsConf.getInt("id") + "";
             //取到对应的财务类型
-            Integer  financeType = -1;
+            /*Integer  financeType = -1;
             for (Integer tempType :  FinanceService.FINANCIAL_TYPE) {
-            	financeType =  getFinancialType(companyId,tempType);
+            	financeType =  getFinancialType(companyId);
             	if(financeType!=-1) {break;}
-			}
-            
-            if(financeType!=-1) {
-            	//财务-表格
-                map.put("financial", financial(financeType+"", finanId,begin,end));
-                //生成财务报告
-                excelPath = financialExcel(financeType+"",finanId,_prePath,orderId,userid,begin,end);
+			}*/
+            List<Integer> financeTypes = getFinancialType(companyId);
+            for(Integer financeType:financeTypes) {
+                //word里财务模块生成
+                financial(financeType, finanId, begin, end, map);
+                //生成财务报告EXCEL
+                String expath = financialExcel(financeType,finanId,_prePath,orderId,userid,begin,end);
+                excelPath.add(expath);
             }
+
             //财务-评价
             map.put("financial_eval", financialEval(statementsConf,reportType,sysLanguage));
-            
         }
 
         //生成word
@@ -434,8 +435,8 @@ public class BaseBusiCrdt {
         List<Map<String, String>> fileList = new ArrayList<>();
         Map<String, String> fileMap = new HashMap();
         fileMap.put(reportName + ".doc",_pre + wordPath);
-        if(!"".equals(excelPath)) {
-            fileMap.put(reportName + ".xls", _pre + excelPath);
+        for(String path:excelPath) {
+            fileMap.put(reportName + ".xls", _pre + path);
         }
         fileList.add(fileMap);
         sendMail(reportName,customId, fileList);
@@ -543,17 +544,18 @@ public class BaseBusiCrdt {
      * @param financialConfId
      * @param begin
      * @param end
+     * @param map
      * @return
      */
-    public static MiniTableRenderData financial(String financeType,String financialConfId,String begin,String end) {
+    public static List<RowRenderData> financial(int financeType,String financialConfId,String begin,String end,HashMap<String, Object> map) {
         List<RowRenderData> rowList = new ArrayList<RowRenderData>();
-        if("3".equals(financeType)){
+        if(financeType==3){
             //todo 大数渲染
         }else{
             //财务
             //取数据
             //Integer type = new ReportInfoGetDataController().getFinanceDictByReportType(reportType);
-            List<CreditCompanyFinancialEntry> finDataRows = FinanceService.getFinancialEntryList(financialConfId, financeType);
+            List<CreditCompanyFinancialEntry> finDataRows = FinanceService.getFinancialEntryList(financialConfId, financeType+"");
             int j = 0;
             Integer old = null;
             for (CreditCompanyFinancialEntry ccf : finDataRows) {
@@ -638,9 +640,10 @@ public class BaseBusiCrdt {
                 rowList.add(RowRenderData.build(new TextRenderData(itemName, sumStyle), new TextRenderData(beginValue.toString()), new TextRenderData(endValue.toString())));
                 j++;
             }
+            //财务-表格
+            map.put("financial", new MiniTableRenderData(rowList));
         }
-
-        return new MiniTableRenderData(rowList);
+        return rowList;
     }
 
     /**
@@ -654,14 +657,14 @@ public class BaseBusiCrdt {
      * @param end
      * @return
      */
-    public static String financialExcel(String financeType,String financialConfId,String _prePath,String orderId,int userid,String begin,String end){
+    public static String financialExcel(int financeType,String financialConfId,String _prePath,String orderId,int userid,String begin,String end){
         String filePath = "";
-        if("3".equals(financeType)){
+        if(financeType==3){
             //todo 大数渲染
         }else{
             //财务
             //Integer type = new ReportInfoGetDataController().getFinanceDictByReportType(reportType);
-            List<CreditCompanyFinancialEntry> finDataRows = FinanceService.getFinancialEntryList(financialConfId, financeType);
+            List<CreditCompanyFinancialEntry> finDataRows = FinanceService.getFinancialEntryList(financialConfId, financeType+"");
             FinancialExcelExport export = new FinancialExcelExport(finDataRows,begin,end);
             try {
                 String path = _prePath + ".xls";
@@ -729,7 +732,6 @@ public class BaseBusiCrdt {
     /**
         * 找到当前公司关联下的,内容不为空的所有财务类型
      * @param companyId
-     * @param type
      * @return  当前公司关联下的,内容不为空的所有财务类型
      */
     public static  List<Integer> getFinancialType (String companyId) {
@@ -739,8 +741,8 @@ public class BaseBusiCrdt {
 		try {
 			// 查询公司id下的财务配置
 			List<CreditCompanyFinancialStatementsConf> flagList = CreditCompanyFinancialStatementsConf.dao.find(
-					"select * from credit_company_financial_statements_conf where del_flag=0 and company_id=?  ",
-					Arrays.asList(new String[] { companyId   }).toArray());
+                    "select * from credit_company_financial_statements_conf where del_flag=0 and company_id=?  ",
+                    Arrays.asList(new String[]{companyId   }).toArray());
             if(flagList==null||flagList.size()==0){
                 return null;
             }
