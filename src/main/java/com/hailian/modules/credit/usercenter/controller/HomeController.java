a1c7b650ec09e9813f75cd15d5b223fad8cd46bc
leizhiguo <1348898564@qq.com>
2019-08-30 16:22:26 +0800

    预览文件路径错误

--- src/main/java/com/hailian/modules/credit/usercenter/controller/HomeController.java
+++ src/main/java/com/hailian/modules/credit/usercenter/controller/HomeController.java
@@ -577,9 +577,14 @@ public class HomeController extends BaseProjectController {
 						String pdf_FTPfileName="";
 						ftpfileList.add(uploadFile.getFile());
 						if(!ext.equals("pdf") && !FileTypeUtils.isImg(ext)&& !ext.equalsIgnoreCase("html")){//如果上传文档不是pdf或者图片则转化为pdf，以作预览
-							File pdf = Office2PDF.toPdf(uploadFile);
-							pdf_FTPfileName=now+"."+"pdf";
-							ftpfileList.add(pdf);
+							try {
+								File pdf = Office2PDF.toPdf(uploadFile);
+								pdf_FTPfileName=originalFileName+now+"."+"pdf";
+								ftpfileList.add(pdf);
+							}catch (Exception e){
+								e.printStackTrace();
+							}
+
 						}else if(ext.equals("pdf") ||FileTypeUtils.isImg(ext)|| ext.equalsIgnoreCase("html")){
 							pdf_FTPfileName=FTPfileName;
 						}
